ec67c2f4ba9e099b53f424b7517f010b60a5ca92
ahwetekm <ahwetekm@gmail.com>
2025-04-03 15:23:20 +0000

    gfmmkgdmdm

---
+++ demo/script.js
@@ -0,0 +1,38 @@
+document.getElementById("noteForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const noteText = document.getElementById("noteText").value;
+    const imageFile = document.getElementById("imageUpload").files[0];
+    
+    if (!noteText && !imageFile) {
+        alert("Lütfen bir not yazın veya bir fotoğraf yükleyin!");
+        return;
+    }
+
+    const data = { text: noteText };
+
+    if (imageFile) {
+        const reader = new FileReader();
+        reader.onload = () => {
+            data.image = reader.result;
+
+            generateQRCode(data);
+        };
+        reader.readAsDataURL(imageFile);
+    } else {
+        generateQRCode(data);
+    }
+});
+
+function generateQRCode(data) {
+    const qrCodeContainer = document.getElementById("qrCodeContainer");
+    qrCodeContainer.innerHTML = ""; // Eski QR kodunu temizler
+    QRCode.toCanvas(
+        JSON.stringify(data), 
+        { width: 250 }, 
+        (err, canvas) => {
+            if (err) console.error(err);
+            qrCodeContainer.appendChild(canvas);
+        }
+    );
+}
