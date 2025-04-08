765099b64b7e4132a7472b5bec5ec1fc05aca24d
ahwetekm <ahwetekm@gmail.com>
2025-04-08 12:02:54 +0000

    64574575545yhteshxed

--- hesap makinesi/hesap_makinesi.py
+++ hesap makinesi/hesap_makinesi.py
@@ -1,1 +1,18 @@
-ıjkklö
+islem = input("Hesaplamak istediğiniz işlemi girin (+, -, *, /): ")
+sayi1 = int(input("Birinci sayıyı giriniz: "))
+sayi2 = int(input("İkinci sayıyı giriniz: "))
+
+if islem == "+":
+    sonuc = sayi1 + sayi2
+    print("Toplama işlemi başarılı. " + str(sonuc))
+elif islem == "-":
+    sonuc = sayi1 - sayi2
+    print("Çıkarma işlemi başarılı. " + str(sonuc))
+elif islem == "*":
+    sonuc = sayi1 * sayi2
+    print("Çarpma işlemi başarılı. " + str(sonuc))
+elif islem == "/":
+    sonuc = sayi1 / sayi2
+    print("Bölme işlemi başarılı. " + str(sonuc))
+else:
+    print("Geçersiz işlem girdiniz.")
