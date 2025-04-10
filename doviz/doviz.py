b4ceefe47f561dbbf4e5977ff0c93c1ac3edf77c
ahwetekm <ahwetekm@gmail.com>
2025-04-10 16:10:56 +0300

    update doviz/doviz.py

--- doviz/doviz.py
+++ doviz/doviz.py
@@ -1,30 +1,43 @@
-usd = 38
-eur = 41
-tl = 1
+#requests kütüphanesini ekledim
+import requests
 
-kur1 = input("Döviz Kuru Giriniz: ")
-kur2 = input("çevireceğimiz Döviz Kurunu Giriniz: ")
-bakiye = int(input("Bakiye Giriniz: "))
+#veri çekmeye başlayalım
+url = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_YJCwRKjN5llJ9CGb0jv3dELGNgxuXRe3pKMPgkph"
+cevap = requests.get(url)
+veri = cevap.json()
 
-if kur1 == "tl" and kur2 == "usd":
-    sonuc = bakiye / usd
-    print("Sonuç: ", sonuc, "Dolar")
-elif kur1 == "tl" and kur2 == "eur":
-    sonuc = bakiye / eur
-    print("sonuç: " + str(sonuc) + " Euro")
-elif kur1 == "usd" and kur2 == "tl":
-    sonuc = bakiye * usd
-    print("Sonuç: ", sonuc, "TL")
-elif kur1 == "eur" and kur2 == "tl":
-    sonuc = bakiye * eur
-    print("Sonuç: ", sonuc, "TL")
+#kurlarımızı tanımlayalım ve api üzerinden kurların değerlerini verelim
+tl = veri['data'] ['TRY']
+eur = veri['data'] ['EUR']
+usd = 1
+
+#kullanıcıya hangi döviz üzerinden işlem yapmak istediğini soralım.
+kur1 = input("Merhaba, gerçek zamanlı kur çeviriciye hoş geldiniz. Bana elinizde hangi döviz bulunduğunu söyler misiniz? (tl, eur, usd): ")
+
+bakiye = int(input("Peki bu elinizdeki dövizin miktarını girer misiniz?: "))
+
+kur2 = input("Hangi dövize çevirmek istersiniz?: ")
+
+#işlemlerimizi yapalım.
+if kur1 == "usd" and kur2 == "tl":
+	net = bakiye * tl
+	print("Kur çevrildi. sizin " + str(round(net, 2)) + " TL'niz var")
 elif kur1 == "usd" and kur2 == "eur":
-    tl = bakiye * usd
-    sonuc = tl / eur
-    print("Sonuç: ", sonuc, "Euro")
+	net = bakiye / eur
+	print("Kur çevrildi. Sizin " + str(round(net, 2)) + " Euro'nuz var.")
 elif kur1 == "eur" and kur2 == "usd":
-    tl = bakiye * eur
-    sonuc = tl / usd
-    print("Sonuç: ", sonuc, "Dolar")
+	net = bakiye / eur
+	print("Kur çevrildi. Sizin " + str(round(net, 2)) + " Dolar'ınız var.")
+elif kur1 == "eur" and kur2 == "tl":
+	usd = bakiye * eur
+	net = usd * tl
+	print("Kur çevrildi. Sizin " + str(round(net, 2)) + " TL'niz var.")
+elif kur1 == "tl" and kur2 == "usd":
+	net = bakiye / tl
+	print("Kur çevrildi. Sizin " + str(round(net, 2)) + " Dolar'ınız var.")
+elif kur1 == "tl" and kur2 == "eur":
+	usd = bakiye / tl
+	net = usd / eur
+	print("Kur çevrildi. Sizin " + str(round(net, 2)) + " Euro'nuz var.")
 else:
-    print("Yanlış döviz kuru girdiniz.")
+	print("Böyle bir kur halihazırda eklenmemiş veya bulunmayabilir. Lütfen bulunduğundan eminseniz bizimle iletişime geçin. Emin değilseniz buyrun tekrar deneyin. ")
