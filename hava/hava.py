724ac61d6d2b8d5c108c98bb9e8d5d566a1139db
ahwetekm <ahwetekm@gmail.com>
2025-04-10 15:34:48 +0000

    uuy

---
+++ hava/hava.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "http://api.weatherapi.com/v1/forecast.json?key=685e2c31dd0c434e92d144440251004&q=Istanbul&days=1&aqi=no&alerts=no"
+gelen = requests.get(url)
+veri = gelen.json()
+ist_durum = veri['current']['condition']['text']
+ist_derece = veri['current']['temp_c']
+print("Istanbul Hava Durumu")
+print("Derece: ", ist_derece)
+print("Durum: ", ist_durum)
+
