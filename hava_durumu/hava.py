b1bd6b37f9140cafbf5e9c91951b7f28331a03b1
ahwetekm <ahwetekm@gmail.com>
2025-04-12 21:15:26 +0300

    denemd

--- hava_durumu/hava.py
+++ hava_durumu/hava.py
@@ -9,7 +9,7 @@ if ist_durum == "Partly cloudy":
   ist_durum = "Parçalı Bulutlu"
 elif ist_durum == "Clear":
   ist_durum = "Açık"
-print("Istanbul Hava Durumu")
+print("Istanbul için hava durumu (genel)")
 print("Derece: ", ist_derece)
 print("Durum: ", ist_durum)
 
