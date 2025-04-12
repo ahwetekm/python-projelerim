aa853abaee1ef0807df73270dccb73aab6a5ee09
ahwetekm <ahwetekm@gmail.com>
2025-04-12 23:24:53 +0300

    düzenleme yapıldı

--- py/deneme.py
+++
@@ -1,7 +0,0 @@
-isim = input("Merhaba, basit mesai hesaplama yazılımına hoş geldiniz. Çalışanın ismini giriniz: ")
-measi = int(input("çalışanın kaç saat mesai yaptığını giriniz: "))
-mu = int(input("çalışan saatlik ne kadar ücret alıyor: "))
-maas = int(input("çalışanın maaşını giriniz: "))
-
-toplam = mu * measi + maas
-print( str(isim) + " Adlı çalışanınızın net maaşı: " + str(toplam))
