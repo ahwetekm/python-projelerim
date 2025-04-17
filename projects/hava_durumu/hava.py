7b5ec0abfd9f0428a98c26ba7f40c57e2cea6e10
ahwetekm <ahwetekm@gmail.com>
2025-04-17 04:58:27 +0300

    hava.py düzenlendi
    Hava durumu uygulamama şehir seçimi geldi ve düzenlemeler yaptı. Kusursuz halde neredeyse

--- projects/hava_durumu/hava.py
+++ projects/hava_durumu/hava.py
@@ -1,17 +1,22 @@
 import requests
 
-url = "http://api.weatherapi.com/v1/forecast.json?key=685e2c31dd0c434e92d144440251004&q=Istanbul&days=1&aqi=no&alerts=no"
+base_url = "http://api.weatherapi.com/v1/forecast.json?key=685e2c31dd0c434e92d144440251004&q="
+sehir = input("Şehir giriniz (Önek: New_York, Istanbul, Ankara, Çankırı, Tokat): ")
+url = (base_url + sehir + "&days=1&aqi=no&alerts=no")
 gelen = requests.get(url)
 veri = gelen.json()
-ist_durum = veri['current']['condition']['text']
-ist_derece = veri['current']['temp_c']
-if ist_durum == "Partly cloudy":
-  ist_durum = "Parçalı Bulutlu"
-elif ist_durum == "Clear":
-  ist_durum = "Açık"
-elif ist_durum == "Sunny":
-  ist_durum = "Güneşli"
-print("Istanbul için hava durumu (genel)")
-print("Derece: ", ist_derece)
-print("Durum: ", ist_durum)
-
+durum = veri['current']['condition']['text']
+derece = veri['current']['temp_c']
+if durum == "Partly cloudy":
+  durum = "Parçalı Bulutlu"
+elif durum == "Clear":
+  durum = "Açık"
+elif durum == "Sunny":
+  durum = "Güneşli"
+elif durum == "Moderate or heavy snow showers":
+  durum = "orta veya yoğun kar yağışlı"
+elif durum == "Heavy snow":
+    durum = "Yoğun kar yağışlı"
+elif durum == "Overcast":
+    durum = "Bulutlu"
+print(sehir," şuan ",str(round(derece, ))," derece ve ",durum)
