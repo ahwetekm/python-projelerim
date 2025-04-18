341b0890d3fdd7572be36891ec97f66080bfd3da
ahwetekm <ahwetekm@gmail.com>
2025-04-18 06:36:20 +0300

    Yorum satırları güncellendi8

--- projects/hava_durumu/hava.py
+++ projects/hava_durumu/hava.py
@@ -3,8 +3,10 @@ import requests
 
 #api üzerinden veri çekiyorum
 base_url = "http://api.weatherapi.com/v1/forecast.json?key=685e2c31dd0c434e92d144440251004&q="
+#Döngü oluşturdum
 while True:
 	islem = input("Yapmak istediğiniz işlemi seçiniz 1[Hava Durumu] 2[çıkış]")
+#"1"i seçerlerse api ile veri çekip hava durumu işlemlerini gerçekleştirelim
 	if islem == "1":
 		sehir = input("Şehir giriniz (Önek: New_York, Istanbul, Ankara, Çankırı, Tokat): ")
 		url = (base_url + sehir + "&days=1&aqi=no&alerts=no")
@@ -13,6 +15,7 @@ while True:
 		durum = veri['current']['condition']['text']
 		derece = veri['current']['temp_c']
 
+#çeviriler
 		if durum == "Partly cloudy":
   			durum = "Parçalı Bulutlu"
 		elif durum == "Clear":
@@ -26,6 +29,7 @@ while True:
 		elif durum == "Overcast":
     			durum = "Bulutlu"
 		print(sehir," şuan ",str(round(derece, )),"°C  ve ",durum)
+#"2"yi seçerlerse metin yazdırıp döngüyü kıralım
 	if islem == "2":
 		print("Çıkışınız sağlandı. Tekrar beklerizz :)")
 		break
